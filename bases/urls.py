73f400f9db9ddf861e18736886a8a300319266ea
CristianVillafan <villafan_cristian@outlook.com>
2023-11-11 07:32:59 -0600

    Hicieron mejoras de response, se agregaron las plantillas, clases y librerias para signin y signup asi como de signout, y se agrego permisos de usuario autenticado

--- bases/urls.py
+++ bases/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Home, PickupOrDelivery
+from .views import Home, PickupOrDelivery, Signup
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', Home.as_view(),name='home'),
-    path('pickup_delivery/', PickupOrDelivery.as_view(), name='pickup_delivery')
+    path('pickup_delivery/', PickupOrDelivery.as_view(), name='pickup_delivery'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('signin/', views.LoginView.as_view(template_name='bases/signin.html'),name='signin'),
+    path('signout', views.LogoutView.as_view(template_name='bases/signin.html'), name='signout')
 ]
--- bases/views.py
+++ bases/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.db import IntegrityError
 
 # Create your views here.
 class Home(generic.TemplateView):
@@ -7,3 +14,11 @@ class Home(generic.TemplateView):
 
 class PickupOrDelivery(generic.TemplateView):
     template_name = 'bases/pickup_or_delivery.html'
+
+class Signup(generic.CreateView):
+    template_name = 'bases/signup.html'
+    context_object_name = 'obj'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('bases:home')
+    def form_valid(self, form):
+        return super().form_valid(form)
